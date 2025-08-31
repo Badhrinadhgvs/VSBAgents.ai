0dcbfbcb0b86cb8ff04be4f9a2a61f17c77afbf3
Venkata Sai Badhrinadh <152195308+Badhrinadhgvs@users.noreply.github.com>
2025-08-31 12:28:55 +0530

    Update settings.py

--- url_generator/settings.py
+++ url_generator/settings.py
@@ -17,6 +17,8 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
 # Quick-start development settings - unsuitable for production
